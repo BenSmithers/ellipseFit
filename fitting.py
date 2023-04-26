0105f2e3b739109958c0817c6eb3f35a750e03dc
Ben Smithers <b.smithers.actual@gmail.com>
2023-04-25 23:02:45 -0500

    run fitter multiple times, higher tolerance

--- fitting.py
+++ fitting.py
@@ -29,6 +29,9 @@ class EllipseFit:
             'maxiter':1e8,
             'maxfev':1e8,
             'maxfun':1e8,
+            'ftol':1e-20,
+            'gtol':1e-20,
+            'eps':1e-20
         }
 
         self._state_file = os.path.join(os.path.dirname(__file__), "ellipse_state.json")
@@ -63,47 +66,51 @@ class EllipseFit:
             print(paramset)
         return False
 
-    def minimize(self):
+    def minimize(self, n_iter = 1):
         """
             Load in the state file (if one was run earlier), perturb it, and run the fitter again
             This is done to hopefully basin-hop while still being in this minimum neighborhood 
         """
-        if os.path.exists(self._state_file) and self._use_previous:
-            _obj = open(self._state_file, 'rt')
-            data  = json.load(_obj)
-            _obj.close()
-            x0 = data["params"]
+        for i in range(n_iter):
+            if os.path.exists(self._state_file) and self._use_previous:
+                _obj = open(self._state_file, 'rt')
+                data  = json.load(_obj)
+                _obj.close()
+                x0 = data["params"]
 
-            x0 = [(1+0.5*np.random.rand())*val for val in x0]
+                x0 = [(1+0.1*np.random.rand())*val for val in x0]
 
-        else:
-            pars = [10.0 for i in range(self._n_dim)]
-            rots = (1.0 + 0.2*np.random.rand(int(self._n_dim*(self._n_dim-1)/2))).tolist()
-            x0 = pars + rots
+            else:
+                pars = [10 for i in range(self._n_dim)]
+                rots = (1.0 + 0.2*np.random.rand(int(self._n_dim*(self._n_dim-1)/2))).tolist()
+                x0 = pars + rots
 
-        result = minimize(self._minfunc, x0, bounds=self.bounds,tol=1e-20, callback = self.callback, options=self.options)
-        params = result.x
+            result = minimize(self._minfunc, x0, bounds=self.bounds, callback = self.callback, options=self.options)
+            
+            print(result)
 
-        data = {
-            "params": [param for param in params],
-            "value":self._minfunc(result.x)
-        }
+            params = result.x
 
-        if not os.path.exists(self._state_file):
-            _obj = open(self._state_file, 'wt')
-            json.dump(data, _obj, indent=4)
-            _obj.close()
-        else:
-            if data["value"] < data["value"]:
-                print("updating fit file!")
+            new_data = {
+                "params": [param for param in params],
+                "value":self._minfunc(result.x)
+            }
 
+            if not os.path.exists(self._state_file):
                 _obj = open(self._state_file, 'wt')
-                json.dump(data, _obj, indent=4)
+                json.dump(new_data, _obj, indent=4)
                 _obj.close()
             else:
-                print("Worse fit... ")
+                if new_data["value"] < data["value"]:
+                    print("updating fit file!")
+
+                    _obj = open(self._state_file, 'wt')
+                    json.dump(new_data, _obj, indent=4)
+                    _obj.close()
+                else:
+                    print("Worse fit... ")
 
 
 if __name__=="__main__":
     fitter = EllipseFit(get_ice_points())
-    fitter.minimize()
+    fitter.minimize(10)
